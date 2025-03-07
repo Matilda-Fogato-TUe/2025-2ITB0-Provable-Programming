610aafd94999561dbc381a4f0bb50f7484bc09c1
Fogato, Matilda <m.fogato@student.tue.nl>
2025-03-07 11:18:46 +0100

    Modified run.sh to work with python3

--- week3/run.sh
+++ week3/run.sh
@@ -3,6 +3,6 @@
 for assignment_file in *-assignment.dfy; do
     if [ -e "$assignment_file" ]; then
         submission_file="${assignment_file%-assignment.dfy}-submission.dfy"
-        python check_submission.py "$assignment_file" "$submission_file"
+        python3 check_submission.py "$assignment_file" "$submission_file"
     fi
 done
